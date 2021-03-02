ce9f3d2a5b9d0d43bdd1fe6eab1d5bea769c02f9
Ilya_MI <ily-khudyakov@yandex.ru>
2021-03-02 19:52:50 +0300

    #1 First commit
    02.03.2021
    Управление джойстиком, модельки, планета со своей гравитацией

---
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+    public float moveSpeed;
+    public DynamicJoystick dynamicJoystick;
+    private Vector3 moveDirection;
+
+
+    void Update()
+    {
+        moveDirection = new Vector3(dynamicJoystick.Horizontal, 0, dynamicJoystick.Vertical).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
+    }
+}
