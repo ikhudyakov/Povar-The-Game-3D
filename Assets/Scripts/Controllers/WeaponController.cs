786dc99b337437818a01dcdcfc40ca302ac798b1
Ilya_MI <ily-khudyakov@yandex.ru>
2021-03-08 23:43:12 +0300

    #2
    Добавлены анимации, передвижение, атака, немного UI.

---
+++ Assets/Scripts/Controllers/WeaponController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public int Damage;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            other.GetComponent<EnemyController>().GetDamage(Damage);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -1,19 +1,75 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
-public class PlayerMovementScript : MonoBehaviour {
+public class PlayerMovementScript : MonoBehaviour
+{
+    UIController uIController;
 
     public float moveSpeed;
     public DynamicJoystick dynamicJoystick;
+    public Animator animator;
+    public Camera _camera;
     private Vector3 moveDirection;
+    private Rigidbody componentRigidbody;
+    private WeaponController weapon;
+    public int allHealth;
+    public int health;
 
 
-    void Update()
+    public int Health { get => health; set => health = value; }
+
+    private void Start()
+    {
+        allHealth = Health = 350;
+        uIController = FindObjectOfType<UIController>();
+        componentRigidbody = GetComponent<Rigidbody>();
+        weapon = FindObjectOfType<WeaponController>();
+        weapon.GetComponent<SphereCollider>().enabled = false;
+        StartCoroutine(TestCoroutine());
+    }
+
+    private IEnumerator TestCoroutine()
+    {
+        while (Health >= 0)
+        {
+            Health -= 15;
+            uIController.ShowHealth((float)Health/((float)allHealth));
+            yield return new WaitForSeconds(3f);
+        }
+
+    }
+
+    private void Update()
     {
         moveDirection = new Vector3(dynamicJoystick.Horizontal, 0, dynamicJoystick.Vertical).normalized;
     }
 
     void FixedUpdate()
     {
-        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
+        
+        Vector3 down = Vector3.Project(componentRigidbody.velocity, transform.up);
+        Vector3 forward = transform.forward * moveSpeed;
+        forward = Vector3.ClampMagnitude(forward, moveSpeed);
+
+        animator.SetFloat("Speed", GetComponent<Rigidbody>().velocity.magnitude);
+
+        if (dynamicJoystick.Horizontal != 0f || dynamicJoystick.Vertical != 0f)
+        {
+            componentRigidbody.velocity = down + forward;
+            transform.LookAt(moveDirection + transform.position);
+        }
+    }
+
+    public void Attack()
+    {
+        animator.SetFloat("Attack", 1.0f);
+        weapon.GetComponent<SphereCollider>().enabled = true;
+    }
+
+    public void StopAttack()
+    {
+        animator.SetFloat("Attack", 0.0f);
+        weapon.GetComponent<SphereCollider>().enabled = false;
     }
 }
